c6d1350abf51428b752c08a989cedee4c30b7ee2
Anvar <xadja35@gmail.com>
2024-02-02 15:56:22 -0500

    props

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import EventListener from '../EventListener'
 import MainNavigation from './MainNavigation'
 
-const Layout: React.FC = ({children}) => {
+const Layout: React.FC = (props: any) => {
   return (
     <>
       <MainNavigation/>
-      <main>{children}</main>
+      <main>{props}</main>
       <EventListener/>
     </>
   )
